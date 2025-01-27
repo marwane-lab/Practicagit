637c9018bc0af80898af69899f6621110aa0b472
Marwane Ajana <maroineajana8@gmail.com>
2025-01-27 00:50:30 +0000

    app de form creado

---
+++ EJpractica/EJpractica/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EJpractica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textoTelegrama;
+            char tipoTelegrama = ' ';
+            int numPalabras = 0;
+            double coste;
+
+            //Leo el telegrama  
+            textoTelegrama = txtTelegrama.Text;
+            // telegrama urgente? 
+            if (chkUrgente.Checked)
+            {
+                tipoTelegrama = 'u';
+            }
+            //Obtengo el número de palabras que forma el telegrama  
+            numPalabras = textoTelegrama.Length;
+ 
+            if (tipoTelegrama == 'o')
+            {
+                if (numPalabras <= 10)
+                {
+                    coste = 2.5;
+                }
+                else
+                {
+                    coste = 0.5 * numPalabras;
+                }
+            }
+            else
+            //Si el telegrama es urgente 
+            {
+                if (tipoTelegrama == 'u')
+                {
+                    if (numPalabras <= 10)
+                    {
+                        coste = 5;
+                    }
+                    else
+                    {
+                        coste = 5 + 0.75 * (numPalabras - 10);
+                    }
+                }
+                else
+                {
+                    coste = 0;
+                }
+            }
+            txtprecio.Text = coste.ToString() + " euros";
+        }
+    }
+            
+    }
+}
